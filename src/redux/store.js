39edd0b156bf514d37f47321c7862c488c937086
OlenaPobehailo <olena.pobehailo@gmail.com>
2023-11-03 19:39:57 +0200

    adds redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import { createStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = createStore(
-	contactsReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+  persistedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
+
+export const persistor = persistStore(store)
